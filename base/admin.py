a0602cb06793d53d869661a8f6b6192ce6790b64
Amauri Rodrigues <amauriaureo@gmail.com>
2023-06-27 07:24:51 -0300

    admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+# É o arquivo que irá indicar quais models ficarão disponíveis
+# no painel administrativo gerado automaticamente pelo Django.
