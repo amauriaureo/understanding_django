f1e51f2c67e511329a6b0704aea10e61691efc12
Amauri Rodrigues <amauriaureo@gmail.com>
2023-06-27 07:22:32 -0300

    models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# São as classes que representam uma tabela no banco de dados.
+# Elas fazem a comunicação de Python com o banco de dados
+# de uma forma mais abstrata
+# (não precisamos escrever o código SQL para consultas, inserções, atualizações e remoções)
