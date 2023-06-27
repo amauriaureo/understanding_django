deeb65af941b6fb46029d5ae78c7081f715f7f1a
Amauri Rodrigues <amauriaureo@gmail.com>
2023-06-27 07:32:44 -0300

    explicando views

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Explicado views.
 # São as funções Python que irão indicar quais funcionalidades têm o sistema,
 # elas irão responder pelas rotas definidas no sistema
