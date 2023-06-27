ed39c52b3fa6964ad89415970ca02bb6b053cbc6
Amauri Rodrigues <amauriaureo@gmail.com>
2023-06-27 07:22:58 -0300

    views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+# São as funções Python que irão indicar quais funcionalidades têm o sistema,
+# elas irão responder pelas rotas definidas no sistema
